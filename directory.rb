de27ec99d335128cd3cef5229dd54a7616090810
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-06 11:53:26 +0100

    Only count names starting with specific letters

--- directory.rb
+++ directory.rb
@@ -8,8 +8,10 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    if name[0].downcase == "s"
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+    end
     # get another name from the user
     name = gets.chomp
   end
