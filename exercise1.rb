95838c35eee6b5047a4276c363082e17f90432d2
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-06 12:02:02 +0100

    Replace .each method with while loop for print method

--- exercise1.rb
+++ exercise1.rb
@@ -8,10 +8,8 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    if name[0].downcase == "s" && name.length <= 12
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-    end
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
@@ -27,10 +25,8 @@ def print_header
 end
 
 def print(students)
-  index = 0
-  while index < students.count do
-    puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
-    index += 1
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
