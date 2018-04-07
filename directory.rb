9661af2324e7b55826b95667dc25d8e0f016ff94
Salome Lambermont <salome.lambermont@gmail.com>
2018-04-07 15:36:31 +0100

    Add save data option

--- directory.rb
+++ directory.rb
@@ -14,6 +14,7 @@ end
 def print_menu
   puts "1. Input students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -28,6 +29,8 @@ def process(selection)
     else
       show_students
     end
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -99,6 +102,19 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students."
+end
+
+# Save the data
+def save_students
+  # Open file for writing
+  file = File.open("students.csv", "w")
+  # Iterate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 # Run program
